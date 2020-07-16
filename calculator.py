a59ab49aa5e16a557ccc920dfb5418cf39b6b860
dair68 <granthuang68@yahoo.com>
2020-07-16 15:15:06 -0700

    changed displayed from label to entry

--- calculator.py
+++ calculator.py
@@ -23,7 +23,8 @@ class Calculator(tk.Frame):
         
     #creates child widgets
     def createWidgets(self):
-        self.display = tk.Label(self, text=" "*20, bg="white", anchor="w")
+        #self.display = tk.Label(self, text=" "*20, bg="white", anchor="w")
+        self.display = tk.Entry(self)
         self.display.grid(row=0, column=0, columnspan=5)
         
         self.enterButton = tk.Button(self, height=1, width=4)
@@ -53,7 +54,7 @@ class Calculator(tk.Frame):
         self.clearButton = tk.Button(self, height=1, width=4)
         self.clearButton["text"] = "clear"
         self.clearButton.grid(row=4, column=4)
-        self.clearButton.bind("<Button-1>", lambda e : self.clearDisplay())
+        self.clearButton.bind("<Button-1>", lambda e : self.display.delete(0, "end"))
         
         self.createNumButtons()
         self.createOperatorButtons()
@@ -92,13 +93,13 @@ class Calculator(tk.Frame):
     def appendSymbol(self, event):
         button = event.widget
         symbol = button["text"]
-        trimmedDisplay = self.display["text"].rstrip()
-        trimmedDisplay += str(symbol)
-        self.updateDisplay(trimmedDisplay)
+        index = self.display.index("insert")
+        self.display.insert(index, symbol)
+        self.display.xview(index)
         
     #evaluates expression and displays answer or error message
     def compute(self):
-        text = self.display["text"].rstrip()
+        text = self.display.get()
         message = ev.evaluate(text)
         print(message)
         
@@ -116,12 +117,5 @@ class Calculator(tk.Frame):
     #updates display with a string. Will leave whitespace unless exceeds 50 chars
     #@param text - string to be displayed
     def updateDisplay(self, text):
-        #display will have trailing whitespace
-        if len(text) < 20:
-            self.display["text"] = text + " "*(20 - len(text))
-        else:
-            self.display["text"] = text
-        
-    #clears all text from display
-    def clearDisplay(self):
-        self.display["text"] = " "*20
+        self.display.delete(0, "end")
+        self.display.insert(0, text)
